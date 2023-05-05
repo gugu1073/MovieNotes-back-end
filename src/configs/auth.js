0b85fac3084203dfd66caf1126566bf4cf4c9ba5
Gustavo <ga2157724@gmail.com>
2023-05-05 16:30:14 -0300

    hash in secret

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
  jwt: {
-  secret: "default",
+  secret: "5f4dcc3b5aa765d61d8327deb882cf99",
   expiresIn: "1d",
  }
 } 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 const migrationRun =  require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
